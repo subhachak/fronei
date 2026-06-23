35ca4a9c7b2d575009cbde50d09a68f064f1b8a7
Subh Chakraborty <subhachak@gmail.com>
2026-06-23 00:22:35 -0400

    Bump brand asset version

--- apps/web/app/lib/brand.ts
+++ apps/web/app/lib/brand.ts
@@ -1,4 +1,4 @@
-export const BRAND_ASSET_VERSION = '20260623-42e2015'
+export const BRAND_ASSET_VERSION = '20260623-43e2015'
 
 export function brandAsset(path: string) {
   return `${path}?v=${BRAND_ASSET_VERSION}`
